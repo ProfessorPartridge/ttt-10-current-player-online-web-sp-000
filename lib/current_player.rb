84ea0076fad46fd95bed2f1d4ebe53655f5ca1fd
Josiah  Partridge <jpartridj94@gmail.com>
2020-01-29 04:20:41 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,7 +9,7 @@ def turn_count(board_array)
 end
 
 def current_player(board_array)
-  if turn_count(board_array) % == 0
+  if turn_count(board_array) % 2 == 0
     return "X"
   else 
     return "O"
